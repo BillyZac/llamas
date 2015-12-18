d8c42017cc4e0dbd4a865d665ba497e539da5967
Noah Roston <rostonn@gmail.com>
2015-12-18 15:20:02 -0700

    knex file

---
+++ migrations/20151218150346_llamas.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('llamas',function(table){
+      table.increments();
+      table.string('name');
+      table.string('image_url');
+  })
+};
+
+exports.down = function(knex, Promise) {
+
+};
